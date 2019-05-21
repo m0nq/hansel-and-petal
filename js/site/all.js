44570ea1214b0b025f63264b173bb03809b34d9c
Monk Wellington <forgetmenaught@gmail.com>
2019-05-20 22:20:48 -0700

    Introduce a prototype chain for code optimization

--- js/site/all.js
+++ js/site/all.js
@@ -637,15 +637,10 @@
     }
   };
 
-  function Arrangement(name, vase, quantity = 1) {
-    this.name = name;
-    this.vase = vase;
-    this.quantity = quantity;
-  }
+  const Item = function () {};
 
-  Arrangement.prototype.type = 'floral';
-  Arrangement.prototype.storage = 'cool';
-  Arrangement.prototype.logItem = function () {
+  Item.prototype.type = 'floral';
+  Item.prototype.logItem = function () {
     console.log(`%c${this.name}`, 'font-weight: bold');
     for (let prop in this) {
       console.log(' ', prop, ': ', this[prop]);
@@ -658,48 +653,38 @@
     this.quantity = quantity;
   }
 
-  Live.prototype.type = 'floral';
+  Live.prototype = new Item();
   Live.prototype.storage = 'warm';
-  Live.prototype.logItem = function () {
-    console.log(`%c${this.name}`, 'font-weight: bold');
-    for (let prop in this) {
-      if (this.hasOwnProperty(prop)) {
-        console.log(' ', prop, ': ', this[prop]);
-      }
-    }
-  };
+
+  function Flower(quantity, color) {
+    this[color] = quantity;
+  }
+
+  Flower.prototype = new Item();
+
+  const Cut = function () {};
+  Cut.prototype = new Item();
+  Cut.prototype.storage = 'cool';
+
+  function Arrangement(name, vase, quantity = 1) {
+    this.name = name;
+    this.vase = vase;
+    this.quantity = quantity;
+  }
+
+  Arrangement.prototype = new Cut();
 
   function Bouquet(name, vase) {
     this.name = name;
     this.vase = vase;
   }
 
-  Bouquet.prototype.type = 'floral';
-  Bouquet.prototype.storage = 'cool';
-  Bouquet.prototype.logItem = function () {
-    console.log(`%c${this.name}`, 'font-weight: bold');
-    for (let prop in this) {
-      if (this.hasOwnProperty(prop)) {
-        console.log(' ', prop, ': ', this[prop]);
-      }
-    }
-  };
+  Bouquet.prototype = new Cut();
+
   Bouquet.prototype.flowers = {
     addStem: function (name, quantity = 1, color = 'Default') {
       this[name] = new Flower(quantity, color);
     }
   };
 
-  function Flower(quantity, color) {
-    this[color] = quantity;
-  }
-
-  Flower.prototype.logItem = function () {
-    console.log(`%c${this.name}`, 'font-weight: bold');
-    for (let prop in this) {
-      if (this.hasOwnProperty(prop)) {
-        console.log(' ', prop, ': ', this[prop]);
-      }
-    }
-  };
 })();
